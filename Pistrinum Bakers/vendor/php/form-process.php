ece885632d1e271d3ef1c261c073012294ba1811
Vamsi Thanjagari <tanjagarivamsi@gmail.com>
2020-05-22 23:45:09 +0530

    Initial Commit

---
+++ Pistrinum Bakers/vendor/php/form-process.php
@@ -0,0 +1,106 @@
+<?php
+namespace PortoContactForm;
+
+session_cache_limiter('nocache');
+header('Expires: ' . gmdate('r', 0));
+
+header('Content-type: application/json');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'php-mailer/src/PHPMailer.php';
+require 'php-mailer/src/SMTP.php';
+require 'php-mailer/src/Exception.php';
+
+// Step 1 - Enter your email address below.
+$email = 'thanjagarivamsi@gmail.com';
+
+// If the e-mail is not working, change the debug option to 2 | $debug = 2;
+$debug = 0;
+
+// If contact form don't has the subject input change the value of subject here
+$subject = ( isset($_POST['subject']) ) ? $_POST['subject'] : 'Message Subject Goes here';
+
+$message = '';
+
+foreach($_POST as $label => $value) {
+	$label = ucwords($label);
+
+	// Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
+
+	// if( $label == 'Email' ) {               
+	// 	$label = 'Email Address';              
+	// }
+
+	// Checkboxes
+	if( is_array($value) ) {
+		// Store new value
+		$value = implode(', ', $value);
+	}
+
+	$message .= $label.": " . htmlspecialchars($value, ENT_QUOTES) . "<br>\n";
+}
+
+$mail = new PHPMailer(true);
+
+try {
+
+	$mail->SMTPDebug = $debug;                                 // Debug Mode
+
+	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
+
+	$mail->IsSMTP();                                         // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';		        		       // Specify main and backup server
+	$mail->SMTPAuth = true;                                  // Enable SMTP authentication
+	
+// *********  This Username and Password is used for testing Purpose. Please do not use this for any other Purpose.*******************************
+//**********  Note :Please Change this Email Id and Password with your email and Don't forget to enable secure apps in your Google account. *************
+
+	$mail->Username = 'vamsi.encrypter@gmail.com';                    // SMTP username 
+      $mail->Password = 'tvamshiniceboy';                              // SMTP password
+	$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
+	$mail->Port = 587 ;   								       // TCP port to connect to
+
+	$mail->AddAddress($email);	 						       // Add another recipient
+
+	//$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
+	//$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
+	//$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
+
+	// From - Name
+	$fromName = ( isset($_POST['from_name']) ) ? $_POST['from_name'] : 'Message From Pistrinum Bakers - Contact Form';
+	$mail->SetFrom($email, $fromName);
+
+	// Repply To
+	if( isset($_POST['for_email']) ) {
+		$mail->AddReplyTo($_POST['from_name'], $fromName);
+	}
+
+	$mail->IsHTML(true);                                       // Set email format to HTML
+
+	$mail->CharSet = 'UTF-8';
+
+	$mail->Subject = $subject;
+	$mail->Body    = $message;
+
+	$mail->Send();
+	$arrResult = array ('response'=>'success');
+	if (!$mail->send())
+{
+   /* PHPMailer error. */
+   echo $mail->ErrorInfo;
+}
+
+} catch (Exception $e) {
+	$arrResult = array ('response'=>'error','errorMessage'=>$e->errorMessage());
+} catch (\Exception $e) {
+	$arrResult = array ('response'=>'error','errorMessage'=>$e->getMessage());
+}
+//
+//if ($debug == 0) {
+	echo json_encode($arrResult);
+//}
+// header("Location:  ../success.php");
+
+?>
